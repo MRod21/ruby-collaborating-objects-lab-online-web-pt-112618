e0bf0ce27334157fba1a122d26d3ed00fa64ca92
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-01-10 04:11:37 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,14 @@
+class MP3Importer
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end
+  
+  def files
+    Dir.glob{"#(@path)/*.mp3"}.map do |file|
+      file.gsub
+  end 
+  
+  
+end
