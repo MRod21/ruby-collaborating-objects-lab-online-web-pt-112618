1b2de301864ed1789ff87fe16b3d77f4ecaf811d
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-01-10 04:34:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,7 @@ class Song
   end
   
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    Artist.name = song_info[0]
-    song
+   
   end
   
 end
